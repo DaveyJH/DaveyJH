4b361440ad07fa1de04590f06f79919bf400a398
DaveyJH <daveyj.dh@googlemail.com>
2021-08-05 14:28:37 +0100

    Mod position of {player}

--- wip/flap.js
+++ wip/flap.js
@@ -3,13 +3,14 @@ let canvasWidth;
 let canvasHeight;
 const ctx = canvas.getContext("2d");
 let rY; //refactor size depending on window height
+let rX; //inset size depending on screen orientation
 
-canvas.style.backgroundColor = "red";
+canvas.style.backgroundColor = "red"; // for demonstration
 
 window.addEventListener("resize", handleChange);
 
 function handleChange() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
   canvasHeight = window.innerHeight;
   canvasWidth = window.innerWidth;
   canvas.width = canvasWidth;
@@ -25,10 +26,15 @@ function handleChange() {
 handleChange();
 
 function calculateSize() {
-  rY = canvasHeight * .155;
+  if (canvasWidth > canvasHeight) {
+    rY = canvasHeight * .155;
+  } else {
+    rY = canvasHeight * .0785;
+  }
+  rX = canvasHeight * .155;
 }
 
 
 function player() {
-  ctx.fillRect(50, 50, 1 * rY, 1 * rY);
+  ctx.fillRect(1 * rX, canvasHeight - 3 * rX, 1 * rY, 1 * rY);
 }
